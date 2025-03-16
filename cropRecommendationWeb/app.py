99a0bd6524ba85d5b0685571ba6b03add02fd9db
Manodi Vijayamini <141545879+rami197@users.noreply.github.com>
2025-03-16 19:57:18 +0530

    Update app.py
    Added Admin model with fields for ID, username, email, and hashed password

--- cropRecommendationWeb/app.py
+++ cropRecommendationWeb/app.py
@@ -73,6 +73,13 @@ class CropRecommendation(db.Model):
     rainfall = db.Column(db.Float, nullable=False)
     selected_crop = db.Column(db.String(100), nullable=True)
 
+#Admin table
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)  # Store hashed passwords
+
 # Feedback Model
 class Feedback(db.Model):
     id = db.Column(db.Integer, primary_key=True)
